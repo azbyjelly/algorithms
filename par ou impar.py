04c59b013357cf19c016f2c583cfbf4c81902c76
jellyboobies <162526784+jellyboobies@users.noreply.github.com>
2024-03-27 20:22:21 -0300

    Update par ou impar.py

--- par ou impar.py
+++ par ou impar.py
@@ -1,35 +1,54 @@
 from random import randint
-vezes_jogadas = int(input("quantas vezes deseja jogar?: "))
-player = 0
-bot = 0
-for i in range(vezes_jogadas):
-    if vezes_jogadas  / 2 + 0.5 <= player or vezes_jogadas  / 2 + 0.5 <= bot:
-        print("puts")
-    else:
-        player = int(input('impar(1) ou par(2)? '))
-        bot = randint(0, 10)
-        numero = int(input('qual numero deseja jogar?'))
-        resultado = (numero + bot)
-        print('bot escolheu {}'.format(bot))
-        if player == 1:
-            if resultado == 1 or resultado == 3 or resultado == 5 or resultado == 7 or resultado == 9 or resulatdo == 11 or resultado == 13 or resultado ==  15 or resultado == 17 or resultado == 19:
-                print('player venceu')
-                player = player + 1
+repeticao = int(input('quantas vezes deseja jogar? '))
+jogador = 0
+maquina = 0
+ganhou = 0
+for i in range(repeticao):
+    if ganhou == 0:
+        if repeticao / 2 + 0.5 <= jogador or repeticao / 2 + 0.5 <= maquina:
+            if jogador > maquina:
+                print('Partida encerrada, pois o jogador venceu')
+                ganhou = 1
             else:
-                print('bot venceu')
-                bot = bot + 1
-        if player == 2:
-            if resultado == 1 or resultado == 3 or resultado == 5 or resultado == 7 or resultado == 9 or reusltado == 11 or resultado == 13 or resultado ==  15 or resultado == 17 or resultado == 19:
-                print('bot venceu')
-                bot = bot + 1
+                print("partida encerrada, pois a máquina venceu")
+                ganhou = 1
+        else:
+            impopar = int(input('impar(1) ou par(2)? '))
+            if impopar < 1 or impopar > 2:
+                print("Escolha entre 1 e 2 apenas")
             else:
-                print('player venceu')
-                player = player + 1
-print('-' * 20)
-print('o player tem {} pontos'.format(player))
-print('bot tem {} pontos'.format(bot))
-if player > bot:
-    print('player ganhou')
+                pc = randint(0, 99)
+                player = int(input('qual numero deseja jogar?'))
+                soma = (player + pc)
+                print(f'a maquina escolheu {pc}')
+                if impopar == 1:
+                    if soma % 2 != 0:
+                        print('jogador venceu')
+                        jogador = jogador + 1
+                    else:
+                        print('maquina venceu')
+                        maquina = maquina + 1
+                if impopar == 2:
+                    if soma % 2 == 0:
+                        print('maquina venceu')
+                        maquina = maquina + 1
+                    else:
+                        print('jogador venceu')
+                        jogador = jogador + 1
+                print('-' * 20)
+print(f'o jogador tem {jogador} pontos')
+print(f'maquina tem {maquina} pontos')
+if jogador == 0 and maquina == 0:
+    print('seu danado fudido ')
 else:
-    print('bot ganhou')
+    if jogador > maquina:
+        print('jogador ganhou')
+    else:
+        if jogador < maquina:
+            print('maquina ganhou')
+        else:
+            print('empatou! ')
 print('-'*20)
+
+#while faz tudo que o for faz mas o for n faz tudo que o while faz
+#usar depurador 
